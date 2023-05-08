0189a5e7f735b2af4c31625ddd0863b9f1a5657c
mymytre <sasha.suliz@gmail.com>
2023-05-08 20:59:03 +0200

    Init

---
+++ src/test/java/dev/hsuliz/bookreviews/BookReviewsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hsuliz.bookreviews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookReviewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
