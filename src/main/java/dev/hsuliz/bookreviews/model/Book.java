102f8d5033e962bb8a03b93dce08519a140142da
mymytre <sasha.suliz@gmail.com>
2023-05-14 15:10:08 +0200

    refactoring

--- src/main/java/dev/hsuliz/bookreviews/model/Book.java
+++ src/main/java/dev/hsuliz/bookreviews/model/Book.java
@@ -1,6 +1,5 @@
 package dev.hsuliz.bookreviews.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
