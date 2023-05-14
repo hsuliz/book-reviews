3e60910bf5a8aab81e15637216d642b617984b89
mymytre <sasha.suliz@gmail.com>
2023-05-14 14:56:56 +0200

    create test for book requester

--- src/test/java/dev/hsuliz/bookreviews/util/component/BookRequesterIntegrationTest.kt
+++ src/test/java/dev/hsuliz/bookreviews/util/component/BookRequesterIntegrationTest.kt
@@ -1,6 +1,8 @@
 package dev.hsuliz.bookreviews.util.component
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import dev.hsuliz.bookreviews.model.Book
+import dev.hsuliz.bookreviews.util.dto.BookResponse
 import dev.hsuliz.bookreviews.util.mapper.BookMapper
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
@@ -44,6 +46,39 @@ class BookRequesterIntegrationTest {
     }
 
     @Test
+    fun `should return book when status 200`() {
+        mockWebServer.enqueue(
+            MockResponse()
+                .addHeader("Content-Type", "application/json")
+                .setResponseCode(200)
+                .setBody(
+                    ObjectMapper().writeValueAsString(
+                        BookResponse(
+                            "9463666656",
+                            "Kees Vuik, Fred Vermolen, Martin van Gijzen",
+                            "Numerical Methods for Ordinary Differential Equations",
+                            "2023",
+                            "https://www.dbooks.org/img/books/9463666656s.jpg"
+                        )
+                    )
+                )
+        )
+
+
+        StepVerifier
+            .create(bookRequester.findById(anyString()))
+            .expectNext(Book(
+                "9463666656",
+                "Kees Vuik, Fred Vermolen, Martin van Gijzen",
+                "Numerical Methods for Ordinary Differential Equations",
+                "2023",
+                "https://www.dbooks.org/img/books/9463666656s.jpg"
+            ))
+            .expectNextCount(1)
+            .verifyComplete()
+    }
+
+    @Test
     fun `should throw when status 404`() {
         mockWebServer.enqueue(
             MockResponse()
@@ -55,3 +90,5 @@ class BookRequesterIntegrationTest {
             .verifyError()
     }
 }
+Book(id=null, externalId=9463666656, title=Kees Vuik, Fred Vermolen, Martin van Gijzen, author=Numerical Methods for Ordinary Differential Equations, year=2023
+Book(id=null, externalId=9463666656, title=Numerical Methods for Ordinary Differential Equations, author=Kees Vuik, Fred Vermolen, Martin van Gijzen, year=2023,
