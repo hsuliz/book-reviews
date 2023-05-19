54a5b95a7f704a4e512e1d5ee717d7290e8fa18d
mymytre <sasha.suliz@gmail.com>
2023-05-19 20:30:34 +0200

    Setup for testcontainers

---
+++ src/test/java/dev/hsuliz/bookreviews/ContainersConfig.kt
@@ -0,0 +1,8 @@
+package dev.hsuliz.bookreviews
+
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+
+@TestConfiguration(proxyBeanMethods = false)
+open class ContainersConfig {
+}
