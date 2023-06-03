1991f761934ec6df826bd0ef5e02f22640095c04
mymytre <sasha.suliz@gmail.com>
2023-06-03 11:04:15 +0200

    Refactoring

--- src/main/java/dev/hsuliz/bookreviews/model/User.java
+++ src/main/java/dev/hsuliz/bookreviews/model/User.java
@@ -5,10 +5,28 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.io.Serial;
 import java.util.Collection;
+import java.util.Objects;
 
 @Document
-public record User(@Id String id, String username, String password) implements UserDetails {
+public final class User implements UserDetails {
+    @Serial
+    private static final long serialVersionUID = 0L;
+
+    @Id
+    private final String id;
+
+    private final String username;
+
+    private final String password;
+
+    public User(String id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
     public User(String username, String password) {
         this(null, username, password);
     }
@@ -46,5 +64,41 @@ public record User(@Id String id, String username, String password) implements U
     @Override
     public boolean isEnabled() {
         return false;
+    }
+
+    @Id
+    public String id() {
+        return id;
+    }
+
+    public String username() {
+        return username;
+    }
+
+    public String password() {
+        return password;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (User) obj;
+        return Objects.equals(this.id, that.id) &&
+                Objects.equals(this.username, that.username) &&
+                Objects.equals(this.password, that.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, password);
+    }
+
+    @Override
+    public String toString() {
+        return "User[" +
+                "id=" + id + ", " +
+                "username=" + username + ", " +
+                "password=" + password + ']';
     }
 }
